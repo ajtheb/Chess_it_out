22c1e0dc517f693d807f8e8d9ec958ec3f0e32c8
Apoorv Jain <apoorvjain.2122@gmail.com>
2022-02-17 21:39:39 +0530

    pawn movement feature

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,3 +1,6 @@
+from importlib_metadata import flake8_bypass
+import pygame
+
 class ChessBoard:
     def __init__(self) -> None:
         self.Board=[
@@ -13,8 +16,76 @@ class ChessBoard:
         self.black_turn=False
         self.white_turn=True
     
-    def move(self,piece_name):
+    def drawBoard(self,gameDisplay):
+        i=0
+        while(i<8):
+            j=0
+            while(j<8):
+                if((i+j)%2==0):
+                    color=	(144,238,144)
+                else:
+                    color=(50,205,50)
+                    
+                pygame.draw.rect(gameDisplay, color,
+                        pygame.Rect(100*j, 100*i, 100, 100))
+                #print(j)
+                j+=1
+            i+=1
+    '''
+    loads the pieces images on corresponding squares
+    '''
+    def loadPieces(self,gameDisplay):
+        for i in [0,1,6,7]:
+            for j in range(len(self.Board[0])):
+                # Creating the image surface
+                image = pygame.image.load('Chess_Pieces\\'+self.Board[i][j][0:2]+".png")
+                image = pygame.transform.scale(image, (100,100))
+
+                # putting our image surface on display
+                # surface
+                gameDisplay.blit(image,(100*j,100*i))
+    '''
+    returns true for white
+    '''
+    def color_check(self,piece_name):
+        if(piece_name[0]=='W'):
+            return True
+        else:
+            return False
+
+    def move(self,piece_name,current,next):
         # move conditions for each peice
-        print("Invalid")
-    
-    
+        allowed=False
+        straight=False
+        diagonal=False
+        back=False
+        left=False
+        right=False
+        x_change=next[0]-current[0]
+        y_change=-1*(next[1]-current[1])
+        if(abs(x_change)>0 and abs(y_change)>0):
+            diagonal=True
+        if(y_change>0 and x_change==0):
+            straight=True
+        if(y_change<0):
+            back=True
+        if((x_change>0 and y_change==0) ):
+            right=True
+        if(x_change<0 and y_change==0):
+            left=True
+        print(piece_name)
+        
+        if(piece_name[0:2]=="WP"):
+            # stright movement
+            #print(back,left,right,straight,x_change,y_change)
+            if(not back and not right and not left and straight and y_change):
+                if(current[1]==6 ):
+                    if(y_change<=2):
+                        allowed=True
+                else:
+                    if(y_change<=1):
+                        allowed=True            
+            if(not back and diagonal):
+                if(self.Board[next[1]][next[0]]!='-' and y_change==1 and abs(x_change)==1):
+                    allowed=True
+        return allowed
--- main.py
+++ main.py
@@ -9,57 +9,60 @@ from pygame.locals import (
     QUIT,
 )
 
-CB=ChessBoard()
 gameDisplay = pygame.display.set_mode((800,800))
-gameDisplay.fill((255,255,255))
+
+CB=ChessBoard()
+CB.drawBoard(gameDisplay=gameDisplay)
+CB.loadPieces(gameDisplay=gameDisplay)
+
+
 running =True
-# pygame.draw.rect(gameDisplay, (255,255,255),
-#                   pygame.Rect(0, 0, 100, 100))
-
-# pygame.draw.rect(gameDisplay, (255,255,255),
-#                   pygame.Rect(200, 0, 100, 100))
-i=0
-while(i<8):
-    j=0
-    while(j<8):
-        if(i%2==0):
-            if( j%2==0):
-                color=	(144,238,144)
-            else:
-                color=(50,205,50)
-        else:
-            if(j%2==0):
-                color=(50,205,50)
-            else:
-                color=(144,238,144)
-        
-        pygame.draw.rect(gameDisplay, color,
-                 pygame.Rect(100*j, 100*i, 100, 100))
-        #print(j)
-        j+=1
-    i+=1
-
-for i in [0,1,6,7]:
-    for j in range(len(CB.Board[0])):
-        # Creating the image surface
-        image = pygame.image.load('Chess_Pieces\\'+CB.Board[i][j][0:2]+".png")
-        image = pygame.transform.scale(image, (100,100))
-
-        # putting our image surface on display
-        # surface
-        gameDisplay.blit(image,(100*j,100*i))
-
-
-pygame.display.flip()
-
-
+piece_selected=False
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
         if event.type == MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
+            if not piece_selected:
+                cur = list(pygame.mouse.get_pos())
+                cur[0]=cur[0]//100
+                cur[1]=cur[1]//100
+                print("current", cur)
+                if(CB.Board[cur[1]][cur[0]]!='-'):
+                    piece_selected=True
+            else:
+                nxt=list(pygame.mouse.get_pos())
+                nxt[0]=nxt[0]//100
+                nxt[1]=nxt[1]//100
+                if(CB.Board[nxt[1]][nxt[0]]!='-' and CB.Board[nxt[1]][nxt[0]]==CB.Board[cur[1]][cur[0]]):
+                    cur[0]=nxt[0]
+                    cur[1]=nxt[1]
+                    print("current", cur)
+                    continue
+                print("next",nxt)
+                if(CB.move(CB.Board[cur[1]][cur[0]],cur,nxt)==True):
+                    print("true")
+                    if((cur[1]+cur[0])%2==0):
+                        color=(144,238,144)
+                    else:
+                        color=(50,205,50)
+                    
+                    #vacanting the current place
+                    pygame.draw.rect(gameDisplay, color,
+                            pygame.Rect(100*cur[0], 100*cur[1], 100, 100))
+                    #print(100*cur[1],100*cur[0])
+                    image = pygame.image.load('Chess_Pieces\\'+CB.Board[cur[1]][cur[0]][0:2]+".png")
+                    image = pygame.transform.scale(image, (100,100))
+                    # putting our image surface on display
+                    # surface
+                    print("put on",nxt)
+                    gameDisplay.blit(image,(100*nxt[0],100*nxt[1]))
+                    temp=CB.Board[cur[1]][cur[0]]
+                    CB.Board[cur[1]][cur[0]]=CB.Board[nxt[1]][nxt[0]]
+                    CB.Board[nxt[1]][nxt[0]]=temp     
+                piece_selected=False
             #highlight_selected_square(get_square_for_position(pos))
         elif event.type == QUIT:
             running = False
+    pygame.display.flip()
