89e441a21d00cc59154d864089c3216c0fe9c7b2
Apoorv Jain <apoorvjain.2122@gmail.com>
2022-02-27 21:50:28 +0530

    Queen ,Rook,Bishop move conditions

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,8 +1,9 @@
-from importlib_metadata import flake8_bypass
+import pathlib
 import pygame
 
 class ChessBoard:
     def __init__(self) -> None:
+        self._path = pathlib.Path("Chess_Pieces")
         self.Board=[
             ["BR1","BN1","BB1","BQ","BK","BB2","BN2","BR2"],
             ["BP1","BP2","BP3","BP4","BP5","BP6","BP7","BP8"],
@@ -34,11 +35,11 @@ class ChessBoard:
     '''
     loads the pieces images on corresponding squares
     '''
-    def loadPieces(self,gameDisplay):
+    def loadPieces(self, gameDisplay):
         for i in [0,1,6,7]:
             for j in range(len(self.Board[0])):
                 # Creating the image surface
-                image = pygame.image.load('Chess_Pieces\\'+self.Board[i][j][0:2]+".png")
+                image = pygame.image.load(self._path / f"{self.Board[i][j][0:2]}.png")
                 image = pygame.transform.scale(image, (100,100))
 
                 # putting our image surface on display
@@ -77,7 +78,7 @@ class ChessBoard:
         right=False # straight right
         x_change=next[0]-current[0]
         y_change=-1*(next[1]-current[1])
-        if(abs(x_change)>0 and abs(y_change)>0):
+        if(abs(x_change)>0 and abs(y_change)>0 and abs(x_change)==abs(y_change)):
             diagonal=True
         if(abs(y_change)>0 and x_change==0):
             straight=True
@@ -87,35 +88,192 @@ class ChessBoard:
             right=True
         if(x_change<0 and y_change==0):
             left=True
+        color=piece_name[0]
+        
+        if(color=='B'):
+            back=not back
+        
         print(piece_name)
         
-        if(piece_name[0:2]=="WP"):
+        if(piece_name[1]=="P"):
             # stright movement
-            #print(back,left,right,straight,x_change,y_change)
+            print(back,left,right,straight,x_change,y_change)
             if(not back and not right and not left and straight and y_change):
-                if(current[1]==6 ):
-                    if(y_change<=2):
+                starting_row=6
+                if(color=='B'):
+                    starting_row=1
+                if(current[1]==starting_row ):
+                    if(abs(y_change)<=2):
                         allowed=True
                 else:
-                    if(y_change<=1):
-                        allowed=True            
+                    if(abs(y_change)<=1):
+                        allowed=True
+                if(self.white_turn):
+                    inc=-1
+                else:
+                    inc=1
+                print(allowed)
+                print(current[1]+inc)
+                for i in range(current[1]+inc,next[1]+inc,inc):
+                    print(i,current[0])
+                    if(self.Board[i][current[0]]!='-'):
+                        allowed=False
+                print(allowed)
+
             if(not back and diagonal):
-                if(self.Board[next[1]][next[0]]!='-' and y_change==1 and abs(x_change)==1):
-                    allowed=True
-        if(piece_name[0:2]=='WN'):
+                # to capture diagonally
+                if(self.Board[next[1]][next[0]]!='-' and abs(y_change)==1 and abs(x_change)==1 ):
+                    if(self.color_check(self.Board[next[1]][next[0]])!=self.color_check(self.Board[current[1]][current[0]])):
+                        allowed=True
+            
+        if(piece_name[1]=='N'):
             if((abs(x_change)==1 and abs(y_change)==2) or (abs(x_change)==2 and abs(y_change)==1)):
                 allowed=True
-        if(piece_name[0:2]=='WB'):
-            if(diagonal==True and not straight):
+        if(piece_name[1]=='B'):
+            # only diagonal movement allowed
+            if(diagonal==True and not straight ):
                 allowed=True
-        if(piece_name[0:2]=='WR'):
+            # Bishop is moving left or right
+            if((next[0]-current[0])>0):
+                inc_horizontal=1
+            else:
+                inc_horizontal=-1
+            # bishop is moving up or down
+            if(self.white_turn):
+                inc_vertical=-1
+            else:
+                inc_vertical=1
+            print(allowed)
+            x_pos=current[0]+inc_horizontal
+            y_pos=current[1]+inc_vertical
+            print(x_pos,y_pos,next[0],next[1],inc_vertical,inc_horizontal)
+            # go through the path and find if there is any obstacle
+            while((inc_vertical==-1 and y_pos>next[1]) or (inc_vertical==1 and y_pos<next[1]) ) \
+                and ((inc_horizontal==-1 and x_pos>next[0]) or (inc_horizontal==1 and x_pos<next[0])):
+                print(x_pos,y_pos)
+                if(self.Board[y_pos][x_pos]!='-'):
+                    allowed=False
+                x_pos+=inc_horizontal
+                y_pos+=inc_vertical
+            
+            print(allowed)
+
+            
+        if(piece_name[1]=='R'):
             if(left==True or right==True or straight==True):
                 allowed=True
-        if(piece_name[0:2]=='WK'):
+            if((next[0]-current[0])>0):
+                inc_horizontal=1
+            else:
+                inc_horizontal=-1
+            # rook is moving up or down
+            if(self.white_turn):
+                inc_vertical=-1
+            else:
+                inc_vertical=1
+            print(allowed)
+            
+            x_pos=current[0]
+            y_pos=current[1]
+            print(x_pos,y_pos)
+            
+            # go through the path and find if there is any obstacle
+
+            if(abs(next[0]-current[0]) >0):
+                x_pos=current[0]+inc_horizontal
+                while((inc_horizontal==-1 and x_pos>next[0]) or (inc_horizontal==1 and x_pos<next[0])):
+                    print(x_pos,y_pos)
+                    if(self.Board[y_pos][x_pos]!='-'):
+                        allowed=False
+                    x_pos+=inc_horizontal
+            
+                    
+            if(abs(next[1]-current[1]) >0):
+                y_pos=current[1]+inc_vertical
+                print(x_pos,y_pos)
+                while((inc_vertical==-1 and y_pos>next[1]) or (inc_vertical==1 and y_pos<next[1]) ):
+                    if(self.Board[y_pos][x_pos]!='-'):
+                        allowed=False
+                    y_pos+=inc_vertical 
+
+            print(x_pos,y_pos,next[0],next[1],inc_vertical,inc_horizontal)
+                      
+        if(piece_name[1]=='K'):
             if(x_change<=1 and y_change<=1):
                 allowed=True
-        if(piece_name[0:2]=='WQ'):
-            if(diagonal or right or straight or left):
+            if(self.Board[next[1]][next[0]][1]=='R'):
                 allowed=True
+            x_pos=current[0]
+            y_pos=current[1]
+            if(self.Board[next[1]][next[0]]=='WR2'):
+                # nothing in between
+                # King side castle
+                if((next[0]-current[0])>0):
+                    inc_horizontal=1
+                else:
+                    inc_horizontal=-1
+                print(y_pos,x_pos+inc_horizontal,y_pos,x_pos+2*inc_horizontal)
+                print(self.Board)
+                if(self.Board[y_pos][(x_pos+inc_horizontal)] !="-" or self.Board[y_pos][(x_pos+2*inc_horizontal)] !="-"):
+                    allowed=False
+            
+                
+
+
+
+        if(piece_name[1]=='Q'):
+            if(diagonal  or right or straight or left):
+                allowed=True
+            if((next[0]-current[0])>0):
+                inc_horizontal=1
+            else:
+                inc_horizontal=-1
+            # bishop is moving up or down
+            if(self.white_turn):
+                inc_vertical=-1
+            else:
+                inc_vertical=1
+            if(diagonal):
+                x_pos=current[0]+inc_horizontal
+                y_pos=current[1]+inc_vertical
+                print(x_pos,y_pos,next[0],next[1],inc_vertical,inc_horizontal)
+                # go through the path and find if there is any obstacle
+                while((inc_vertical==-1 and y_pos>next[1]) or (inc_vertical==1 and y_pos<next[1]) ) \
+                    and ((inc_horizontal==-1 and x_pos>next[0]) or (inc_horizontal==1 and x_pos<next[0])):
+                    print(x_pos,y_pos)
+                    if(self.Board[y_pos][x_pos]!='-'):
+                        allowed=False
+                    x_pos+=inc_horizontal
+                    y_pos+=inc_vertical
+            
+            if(right or left or straight):
+                x_pos=current[0]
+                y_pos=current[1]
+                print(x_pos,y_pos)
+                
+                # go through the path and find if there is any obstacle
+
+                if(abs(next[0]-current[0]) >0):
+                    x_pos=current[0]+inc_horizontal
+                    while((inc_horizontal==-1 and x_pos>next[0]) or (inc_horizontal==1 and x_pos<next[0])):
+                        print(x_pos,y_pos)
+                        if(self.Board[y_pos][x_pos]!='-'):
+                            allowed=False
+                        x_pos+=inc_horizontal
+                
+                        
+                if(abs(next[1]-current[1]) >0):
+                    y_pos=current[1]+inc_vertical
+                    print(x_pos,y_pos)
+                    while((inc_vertical==-1 and y_pos>next[1]) or (inc_vertical==1 and y_pos<next[1]) ):
+                        if(self.Board[y_pos][x_pos]!='-'):
+                            allowed=False
+                        y_pos+=inc_vertical 
+
         
         return allowed
+    
+    # def obstacle_check(self,piece_name,current,next):
+    #     if(piece_name[1]=="P"):
+
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from ChessBoard import ChessBoard
-import pygame 
+import pygame
+import pathlib
+
 pygame.init()
 
 from pygame.locals import (
@@ -9,15 +11,15 @@ from pygame.locals import (
     QUIT,
 )
 
-gameDisplay = pygame.display.set_mode((800,800))
+gameDisplay = pygame.display.set_mode((800, 800))
 
-CB=ChessBoard()
+CB = ChessBoard()
 CB.drawBoard(gameDisplay=gameDisplay)
 CB.loadPieces(gameDisplay=gameDisplay)
 
 
-running =True
-piece_selected=False
+running = True
+piece_selected = False
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -26,47 +28,77 @@ while running:
         if event.type == MOUSEBUTTONUP:
             # no piece selected at the moment
             if not piece_selected:
+
                 cur = list(pygame.mouse.get_pos())
-                cur[0]=cur[0]//100
-                cur[1]=cur[1]//100
+                cur[0] = cur[0] // 100
+                cur[1] = cur[1] // 100
                 print("current", cur)
-                if(CB.Board[cur[1]][cur[0]]!='-'):
-                    piece_selected=True
+                if (CB.color_check(CB.Board[cur[1]][cur[0]]) and CB.white_turn) or (
+                    not CB.color_check(CB.Board[cur[1]][cur[0]]) and CB.black_turn
+                ):
+                    if CB.Board[cur[1]][cur[0]] != "-":
+                        piece_selected = True
             else:
-                nxt=list(pygame.mouse.get_pos())
-                nxt[0]=nxt[0]//100
-                nxt[1]=nxt[1]//100
-                # next piece is not empty and it is equal to itself
-                if(CB.Board[nxt[1]][nxt[0]]!='-' and CB.Board[nxt[1]][nxt[0]]==CB.Board[cur[1]][cur[0]]):
-                    # selection of piece to move is changed
-                    cur[0]=nxt[0]
-                    cur[1]=nxt[1]
+                nxt = list(pygame.mouse.get_pos())
+                nxt[0] = nxt[0] // 100
+                nxt[1] = nxt[1] // 100
+                castle=False
+                if(CB.Board[cur[1]][cur[0]][1]=='K' and CB.Board[nxt[1]][nxt[0]][1]=='R'):
+                    castle=True
+                # next piece is not empty and piece colour is same
+                if CB.Board[nxt[1]][nxt[0]] != "-" and CB.color_check(CB.Board[nxt[1]][nxt[0]]) == CB.color_check(CB.Board[cur[1]][cur[0]]) and not castle:
+                    # selection of moving piece is changed
+                    print("selection of moving piece is changed")
+                    cur[0] = nxt[0]
+                    cur[1] = nxt[1]
                     print("current", cur)
                     continue
-                print("next",nxt)
+                print("next", nxt)
 
                 # check if move is allowed
-                if(CB.move(CB.Board[cur[1]][cur[0]],cur,nxt)==True):
+                if CB.move(CB.Board[cur[1]][cur[0]], cur, nxt) == True:
                     print("true")
-                    color=CB.which_color(cur[1],cur[0])
-                    #vacanting the current place
-                    pygame.draw.rect(gameDisplay, color,
-                            pygame.Rect(100*cur[0], 100*cur[1], 100, 100))
-                    #print(100*cur[1],100*cur[0])
-                    image = pygame.image.load('Chess_Pieces\\'+CB.Board[cur[1]][cur[0]][0:2]+".png")
-                    image = pygame.transform.scale(image, (100,100))
-                    
-                    print("put on",nxt)
-                    color=CB.which_color(nxt[1],nxt[0])
-                    print(color)
-                    pygame.draw.rect(gameDisplay, color,
-                            pygame.Rect(100*nxt[0], 100*nxt[1], 100, 100))
-                    gameDisplay.blit(image,(100*nxt[0],100*nxt[1]))
-                    temp=CB.Board[cur[1]][cur[0]]
-                    CB.Board[cur[1]][cur[0]]=CB.Board[nxt[1]][nxt[0]]
-                    CB.Board[nxt[1]][nxt[0]]=temp     
-                piece_selected=False
-            #highlight_selected_square(get_square_for_position(pos))
+                    color = CB.which_color(cur[1], cur[0])
+                    # vacanting the current place
+                    pygame.draw.rect(
+                        gameDisplay,
+                        color,
+                        pygame.Rect(100 * cur[0], 100 * cur[1], 100, 100),
+                    )
+                    image = pygame.image.load(CB._path / f"{CB.Board[cur[1]][cur[0]][0:2]}.png")
+                    image = pygame.transform.scale(image, (100, 100))
+
+                    if castle==True :
+                        color = CB.which_color(cur[1], cur[0]+2)
+                        print(color)
+                        print(nxt)
+                        pygame.draw.rect(
+                            gameDisplay,
+                            color,
+                            pygame.Rect(100 * (cur[0]+2), 100 * cur[1], 100, 100),
+                        )
+                        gameDisplay.blit(image, (100 *(cur[0]+2), 100 * cur[1]))
+                    else:    
+                        
+                        # print(100*cur[1],100*cur[0])
+                        
+                        print("put on", nxt)
+                        color = CB.which_color(nxt[1], nxt[0])
+                        print(color)
+                        pygame.draw.rect(
+                            gameDisplay,
+                            color,
+                            pygame.Rect(100 * nxt[0], 100 * nxt[1], 100, 100),
+                        )
+                        gameDisplay.blit(image, (100 * nxt[0], 100 * nxt[1]))
+                        temp = CB.Board[cur[1]][cur[0]]
+                        CB.Board[cur[1]][cur[0]] = CB.Board[nxt[1]][nxt[0]]
+                        CB.Board[nxt[1]][nxt[0]] = temp
+                    CB.black_turn = not CB.black_turn
+                    CB.white_turn = not CB.white_turn
+
+                piece_selected = False
+            # highlight_selected_square(get_square_for_position(pos))
         elif event.type == QUIT:
             running = False
     pygame.display.flip()
